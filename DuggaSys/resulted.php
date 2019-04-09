272a3cd679f996ee01240ccc587020bb9bc33736
c17johha <c17johha@student.his.se>
2019-04-09 13:44:51 +0200

    Update resulted.php

--- DuggaSys/resulted.php
+++ DuggaSys/resulted.php
@@ -58,14 +58,14 @@ pdoConnect();
         <label>Betygsskala</label>
         <!--<input id="ladgradescale" type="text" style="font-size:12px;">-->
         <select id="ladgradescale">
-          <option value="U-G-VGgg" selected>U-G-VG</option>
-          <option value="U-GGG">U-G</option>
-          <option value="U-3-4-577">U-3-4-5</option>
+          <option value="U-G-VG" selected>U-G-VG</option>
+          <option value="U-G">U-G</option>
+          <option value="U-3-4-5">U-3-4-5</option>
         </select>
       </div>
       <div style="display:flex;flex-direction:column;justify-content:space-between;margin:5px;">
         <label>Betygsdatum</label>
-        <input id="laddate" type="date" style="font-size:12px;">
+        <input id="laddate" type="date" style="font-size:18px;">
       </div>
       <button onclick="ladexport();">LadExport</button>
     </div>
