4a78fd672e7f4ffa49ca8a4af9d7aa4543a7a512
b17nicsv <49198878+b17nicsv@users.noreply.github.com>
2019-04-09 13:40:56 +0200

    Update sectioned.php

--- DuggaSys/sectioned.php
+++ DuggaSys/sectioned.php
@@ -106,11 +106,11 @@
 					</td>
 					<td class='access menuButton' style='display: inline-block;'>
 						<div class='access menuButton'>
-							<input type='button' value='hej kom o hjälp mig' class='submit-button' title='Give students access to the selected version' onclick='navigatePage("accessed.php");'/>
+							<input type='button' value='Access' class='submit-button' title='Give students access to the selected version' onclick='navigatePage("accessed.php");'/>
 						</div>
 					</td>
-					<td class='mail menuButton' style='display: inline-block;'>
-						<div class='mail menuButton'>
+					<td class='access menuButton' style='display: inline-block;'>
+						<div class='access menuButton'>
 							<input type='button' value='Mail' class='submit-button' title='TEST MAIL' onclick='navigatePage("accessed.php");'/>
 						</div>
 					</td>
